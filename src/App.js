c1e95191c17639e5e7f1611b832f7da0b929ff6d
Burhan Maseel <burhanmaseel@gmail.com>
2019-03-25 20:06:19 +0500

    First Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>this is react app</h1>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
     // return React.createElement('div',null,React.createElement('h1', {className: 'App'}, 'This is react app'));
